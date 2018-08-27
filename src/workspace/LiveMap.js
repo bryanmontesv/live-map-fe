46a89539aba49c9940658c4a5f2e6da490b1bad7
Bryan Montes <jmontes@foundationmedicine.com>
2018-08-27 02:47:48 -0500

    feature/lmf-2 Added post and put functionality on every marker inside the map, added new components for web forms, added new button on the top right to toggle a web form, added bunch of new functionality

--- src/workspace/LiveMap.js
+++ src/workspace/LiveMap.js
@@ -1,75 +1,70 @@
 import React from 'react'
 import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from 'react-mapbox-gl'
+import EditLocationForm from '../components/map/EditLocationForm'
+import PopupFormat from '../components/map/PopupFormat'
+import AddLocationForm from '../components/map/AddLocationForm'
 import config from '../config/index'
-import styled from 'styled-components'
-import moment from 'moment'
-import LocationForm from '../components/locationForm'
-
 import axios from 'axios'
-import '../css/map.css'
+import moment from 'moment'
+import styled from 'styled-components'
 
-const StyledPopup = styled.div`
+const PopupStyle = styled.div`
   background: white;
   color: #978ed1;
-  padding: 5px;
+  padding: 6px;
   border-radius: 2px;
 `;
 
-// this token is taken from internet. 
+// this token was taken from internet :) 
 const Map = ReactMapboxGl({ accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA' })
 class LiveMap extends React.Component {
-  
-  constructor(props: Props) {
+  constructor(props) {
     super(props);
     this.state = {
       center: [-103.3359, 20.6576],
       zoom: 11.16,
       locations: [],
       location: undefined,
-      isEditing: false
+      addLocation: {},
+      isEditing: false,
+      isAddingLocation: false
     };
   }
 
-  // Extract all locations from database
+  /**
+   * Extract all locations from database when the entire map is running well
+   */
   componentDidMount() {
     axios.get(`${config.lmbApi}/api/locations`)
       .then((response) => {
-        console.log('response', response)
         this.setState({ locations: response.data })
       })
       .catch(error => {
-        console.log(error)
+        throw error
       })
   }
 
   // I use this function just for closing a pop-up if there is a location assigned
-  onMapMove (e) {
+  onMapMove () {
     if (this.state.location) {
       this.setState({ location: undefined })
     }
-    console.log(e)
-  }
-
-  /**
-   * This function will let you know if at your current time the location is opened or not
-   * @param {String} open_time location open time
-   * @param {String} close_time location close time 
-   */
-  locationAvailability (open_time, close_time) {
-    const open_at = moment(open_time, 'HH:mm')
-    const close_at = moment(close_time, 'HH:mm')
-    return moment().isBetween(open_at, close_at)
   }
 
   // When you click on a marker we should set a location into the state to open a pop-up
-  onMarkerClicked (location, e) {
+  onMarkerClicked (location) {
     this.setState({
-      // center: [e.lngLat.lng, e.lngLat.lat], // check this behavior
-      location
+      // center: [e.lngLat.lng, e.lngLat.lat], // check this behavior because its moving the map closing my popups!!
+      location,
+      isEditing: false
     })
   }
 
-  // when you drag a marker to another place it should be stored on the database.
+  /**
+   *  When you drag a marker to another place it should be stored on the database.
+   * @param {Object} location current location object which is moving around the map
+   * @param {Object} e element event
+   */
   onDragEndMarker (location, e) {
     axios.put(`${config.lmbApi}/api/locations`, {
       id: location.id,
@@ -84,10 +79,10 @@ class LiveMap extends React.Component {
         }
         return stateLocation
       })
-      this.setState({ locations: updateStateLocations, location: undefined })
+      this.setState({ locations: updateStateLocations, location: undefined, isEditing: false })
     })
     .catch(err => {
-      console.log('Error trying to update: ', err)
+      throw err
     })
   }
 
@@ -95,11 +90,21 @@ class LiveMap extends React.Component {
   onMouseHover (cursor: string, { map }: { map: any }) {
     map.getCanvas().style.cursor = cursor;
   }
-
+  /**
+   * Simple function to know which view should the user see when clicking on Edit or Add Location button
+   */
   editLocation () {
     this.setState({ isEditing: !this.state.isEditing })
   }
 
+  toggleAddingLocation() {
+    this.setState({ isAddingLocation: !this.state.isAddingLocation })
+  }
+
+  /**
+   * This method allow the application to modify a location
+   * @param {Object} event form event object
+   */
   onSubmitLocation (event) {
     let { longitude, latitude, location_name, open_time, close_time, id } = this.state.location
 
@@ -111,83 +116,131 @@ class LiveMap extends React.Component {
       open_time: moment(open_time, 'HH:mm').format(),
       close_time: moment(close_time, 'HH:mm').format(),
     })
-      .then(response => {
-        const updatedLocation = response.data[0]
-        let updateStateLocations = this.state.locations.map(stateLocation => {
-          if (stateLocation.id === id) {
-            return updatedLocation
-          }
-          return stateLocation
-        })
-        this.setState({ locations: updateStateLocations, isEditing: !this.state.isEditing })
+    .then(response => {
+      const updatedLocation = response.data[0]
+      let updateStateLocations = this.state.locations.map(stateLocation => {
+        if (stateLocation.id === id) {
+          return updatedLocation
+        }
+        return stateLocation
       })
-      .catch(err => {
-        console.log('Error trying to update: ', err)
-      })
+      this.setState({ locations: updateStateLocations, isEditing: !this.state.isEditing })
+    })
+    .catch(err => {
+      throw err
+    })
     event.preventDefault();
   }
 
+  /**
+   * 
+   * @param {Object} event onChange function sent from web location form
+   */
   onLocationValueChange (event) {
     let location = this.state.location || {}
     location[event.target.name] = event.target.value
     this.setState({ location })
   }
 
+  /**
+   * This method allow the application to submit new locations into the map
+   * @param {Object} event form event object
+   */
+  onSubmitNewLocation (event) {
+    event.preventDefault();
+    let { longitude, latitude, location_name, open_time = moment().format('HH:mm'), close_time = moment().format('HH:mm') } = this.state.addLocation
+
+    axios.post(`${config.lmbApi}/api/locations`, {
+      longitude: longitude.toString(),
+      latitude: latitude.toString(),
+      location_name,
+      open_time: moment(open_time, 'HH:mm').format(),
+      close_time: moment(close_time, 'HH:mm').format(),
+    })
+    .then(response => {
+      this.setState({
+        locations: [...this.state.locations, ...response.data],
+        isEditing: false,
+        isAddingLocation: false,
+        addLocation: {}
+      })
+    })
+    .catch(err => {
+      throw err
+    })
+  }
+
+  /**
+   * This method allow the app to store properties from a new location
+   * @param {Object} event form event object
+   */
+  onAddLocationChange (event) {
+    let addLocation = this.state.addLocation || {}
+    addLocation[event.target.name] = event.target.value
+    this.setState({ addLocation })
+  }
+
   render() {
-    const { center, zoom, location, isEditing } = this.state;
+    const { center, zoom, location, isEditing, isAddingLocation } = this.state;
     return (
-      <Map
-        containerStyle={{
-          height: '98vh',
-          width: '100%'
-        }}
-        style='mapbox://styles/mapbox/navigation-guidance-day-v2'
-        center={center}
-        zoom={[zoom]}
-        onMoveStart={this.onMapMove.bind(this)} >
-        <ZoomControl position="top-left" />
-        <a href='/' className='button icon plus loud top'>Add</a>
-        <Layer 
-          type="symbol"
-          id="marker"
-          layout={{ "icon-image": "circle-15" }} >
-            {this.state.locations.map((location) => (
-              <Feature key={location.id}
-                onMouseEnter={this.onMouseHover.bind(this, 'pointer')}
-                onMouseLeave={this.onMouseHover.bind(this, '')}
-                onDragEnd={this.onDragEndMarker.bind(this, location)}
-                coordinates={[location.longitude, location.latitude]}
-                onClick={this.onMarkerClicked.bind(this, location)}
-                draggable={true}
-               />
-            ))}
-        </Layer>
-        {location &&
-          <Popup
-            key={location.id}
-            offset={8}
-            coordinates={[location.longitude, location.latitude]} >
-            <StyledPopup>
-              {isEditing ?
-              <LocationForm 
-              onSubmitForm={this.onSubmitLocation.bind(this)}
-              onChange={this.onLocationValueChange.bind(this)}
-              locationValues={location} />
-              :
-              <div>
-                <label onClick={this.editLocation.bind(this)}>Edit</label>
-                <div> Location Name: {location.location_name} </div>
-                <div> Opens at: {location.open_time} </div>
-                <div> Closes at: {location.close_time} </div>
-                <div>
-                  Currently: {this.locationAvailability(location.open_time, location.close_time) ? 'Open' : 'Closed'}
-                </div>
-              </div>
-              }
-            </StyledPopup>
-          </Popup>
+      <div>
+        {isAddingLocation && 
+          <AddLocationForm
+            onSubmitForm={this.onSubmitNewLocation.bind(this)}
+            onChange={this.onAddLocationChange.bind(this)}
+            addLocationValues={this.state.addLocation} />
         }
-      </Map>
+        <Map
+          containerStyle={{
+            height: '98vh',
+            width: '100%'
+          }}
+          style='mapbox://styles/mapbox/navigation-guidance-day-v2'
+          center={center}
+          zoom={[zoom]}
+          onMoveStart={this.onMapMove.bind(this)} >
+          <ZoomControl position="top-left" />
+          <button
+            className='button overMap'
+            onClick={this.toggleAddingLocation.bind(this)} >
+            {isAddingLocation ? 'Close Form' :'Add Location'}
+          </button>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "circle-15" }} >
+              {this.state.locations.map((location) => (
+                <Feature key={location.id}
+                  onMouseEnter={this.onMouseHover.bind(this, 'pointer')}
+                  onMouseLeave={this.onMouseHover.bind(this, '')}
+                  onDragEnd={this.onDragEndMarker.bind(this, location)}
+                  coordinates={[location.longitude, location.latitude]}
+                  onClick={this.onMarkerClicked.bind(this, location)}
+                  draggable={true}
+                />
+              ))}
+          </Layer>
+          {location &&
+            <Popup
+              key={location.id}
+              offset={8}
+              coordinates={[location.longitude, location.latitude]} >
+              <PopupStyle>
+                {isEditing ?
+                <EditLocationForm 
+                  onSubmitForm={this.onSubmitLocation.bind(this)}
+                  onChange={this.onLocationValueChange.bind(this)}
+                  locationValues={location} />
+                :
+                <PopupFormat 
+                  onClick={this.editLocation.bind(this)}
+                  locationValues={location} />
+                }
+              </PopupStyle>
+            </Popup>
+          }
+        </Map>
+      </div>
     )
   }
 }
